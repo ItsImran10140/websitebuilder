c18a2f2842abe772eee04fa490b2aea6034d6436
Imran Shah <imranshah10140@gmail.com>
2024-12-04 14:18:03 +0530

    Added the Complete Webside Builder with Payment Gatway and Account Dashboard and SubAccount Dashboard

---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/components-tab/text-placeholder.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EditorBtns } from "@/lib/constants";
+import { TypeIcon } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const TextPlaceholder = (props: Props) => {
+  const handleDragState = (e: React.DragEvent, type: EditorBtns) => {
+    if (type === null) return;
+    e.dataTransfer.setData("componentType", type);
+  };
+
+  return (
+    <div
+      draggable
+      onDragStart={(e) => {
+        handleDragState(e, "text");
+      }}
+      className=" h-14 w-14 bg-muted rounded-lg flex items-center justify-center"
+    >
+      <TypeIcon size={40} className="text-muted-foreground" />
+    </div>
+  );
+};
+
+export default TextPlaceholder;
